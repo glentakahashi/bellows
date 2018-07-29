d5eaa636334c9c0c91ef8da2e52f05ad0ea0f18a
Glen Takahashi <me@glentaka.com>
2018-07-29 02:53:46 -0400

    Simplify reset

--- bellows/uart.py
+++ bellows/uart.py
@@ -145,7 +145,7 @@ class Gateway(asyncio.Protocol):
     def error_frame_received(self, data):
         """Error frame receive handler"""
         LOGGER.debug("Error frame: %s", binascii.hexlify(data))
-        self.reset()
+        self.write(self._rst_frame())
 
     def write(self, data):
         """Send data to the uart"""
